050a990b5bf6c00975022205284ca53edf2b0c8f
Hyunji <6120hj@gmail.com>
2023-08-11 14:15:56 +0900

    Docs: add exercise1.py

---
+++ Day2/exercise1.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this line 👇
+str = str(two_digit_number);
+str1 = str[0];
+str2 = str[1];
+
+print(int(str1) + int(str2));
