63c9dd9d32e23e19de4b4446c3a92b931bce666b
Hyunji <6120hj@gmail.com>
2023-08-11 14:18:20 +0900

    Docs: add tip-calculator.py

---
+++ Day2/tip-calculator.py
@@ -0,0 +1,7 @@
+#Write your code below this line 👇
+print("Welcome to the tip calculator!");
+total = float(input("What was the total bill? $"));
+tip_percent = int(input("How much tip would you like to give? 10, 12, or 15? "));
+num = int(input("How many people to split the bill? "));
+
+print(f"Each person should pay: ${round((total*(1+(tip_percent/100)))/num, 2)}");
