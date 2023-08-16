30f426362834cc74658039d93fd3f5329e21bbda
Hyunji <6120hj@gmail.com>
2023-08-16 16:10:30 +0900

    Docs: add exercise3.py

---
+++ Day5/exercise3.py
@@ -0,0 +1,14 @@
+#Write your code below this row 👇
+total = 0;
+for even_num in range(2, 101, 2):
+    total += even_num;
+print(total);
+
+
+#Write your code below this row 👇
+total2 = 0;
+for num in range(1,101):
+    if num % 2 == 0:
+        total2 += num;
+
+print(total2);
